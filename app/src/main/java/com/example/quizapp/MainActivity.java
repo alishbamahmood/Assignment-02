e7fdbed44a9fbf7c6cc0ba56b8a194ad6cb62a3d
Alishba Mahmood <bcsf19a029@pucit.edu.pk>
2022-11-12 09:44:41 +0500

    Commit-2

--- app/src/main/java/com/example/quizapp/MainActivity.java
+++ app/src/main/java/com/example/quizapp/MainActivity.java
@@ -4,11 +4,25 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import android.view.View;
+import android.widget.TextView;
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-    }
+import java.util.*;
+
+
+public class MainActivity extends AppCompatActivity{
+
+    List<List<String>> lists = new ArrayList<List<String>>();
+
+    String[][] ques
+            = {
+            {"There are _________ types of bonds","2","3","4"},
+            {"The right most position in the periodic table is occupied by","2","3","4"},
+            {"It is a triatomic molecule","2","3","4"}
+    };
+
+    TextView textView;
+    textView = findViewById(R.id.que);
+
+
 }
