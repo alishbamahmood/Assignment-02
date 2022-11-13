fd7c44e26dc6693d6c1d2c8439c693305f3e9242
Alishba Mahmood <bcsf19a029@pucit.edu.pk>
2022-11-13 08:56:52 +0500

    Commit-6

--- app/src/main/java/com/example/quizapp/MainActivity.java
+++ app/src/main/java/com/example/quizapp/MainActivity.java
@@ -11,7 +11,7 @@ import android.widget.TextView;
 import java.util.*;
 
 
-public class MainActivity extends AppCompatActivity{
+public class MainActivity extends AppCompatActivity {
 
     TextView course, que;
     Button btn1, btn2, btn3, btn4;
@@ -20,10 +20,14 @@ public class MainActivity extends AppCompatActivity{
 
     Random rnd;
     int score,rndNumber;
+    int i=0;
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState)
-    {
+    Integer[] selected = new Integer[]{};
+
+    List<Integer> selectedList = new ArrayList<>(Arrays.asList(selected));
+
+
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
@@ -35,22 +39,108 @@ public class MainActivity extends AppCompatActivity{
         btn3 = findViewById(R.id.opt3);
         btn4 = findViewById(R.id.opt4);
 
-        quizQues.add(new Quiz("There are _________ types of bonds","2","3","4","5","4"));
-        quizQues.add(new Quiz("In silver plating Anode is made of","Gold","Ag","Neon","Nil","Ag"));
-        quizQues.add(new Quiz("All metals are solid except","Al","Mg","Na","Hg","Hg"));
-        quizQues.add(new Quiz("Neutrons have _________ charge.","+","-","No","Nil","-"));
-        quizQues.add(new Quiz("Bucky Balls are used as","Lubricants","Cutting","Crafting","Nil","Lubricants"));
-        quizQues.add(new Quiz("Diamonds is ____________","Soft","Hard","Bulky","Nil","Hard"));
-        quizQues.add(new Quiz("Mass per unit volume is:","Velocity","Density","Pressure","Nil","Density"));
-        quizQues.add(new Quiz("The forces of attraction present between the molecules of a substance are called ____________ forces.","Covalent","Ionic","Intermolecular","Nil","Intermolecular"));
-        quizQues.add(new Quiz("The earth is made up of ___________ % of iron.","4","5","6","7","5"));
-        quizQues.add(new Quiz("____________ is an irreversible cell.","Dry Cell","Zn Cell","Chargeable Cell","None of these","Dry Cell"));
+        String[] options1 =  new String[]{"2","3","4","5"};
+        quizQues.add(new Quiz("There are _________ types of bonds",options1,"4"));
 
-        rndNumber = rnd.nextInt(quizQues.size());
+        String[] options2 =  new String[]{"Gold","Ag","Neon","Nil"};
+        quizQues.add(new Quiz("In silver plating Anode is made of",options2,"Ag"));
 
-        que.setText(quizQues.get(rndNumber).getQue());
-        btn1.setText(quizQues.get(rndNumber).getOpt1());
-        btn2.setText(quizQues.get(rndNumber).getOpt2());
-        btn3.setText(quizQues.get(rndNumber).getOpt3());
+        String[] options3 =  new String[]{"Al","Mg","Na","Hg"};
+        quizQues.add(new Quiz("All metals are solid except",options3,"Hg"));
+
+        String[] options4 =  new String[]{"+","-","No","Nil"};
+        quizQues.add(new Quiz("Neutrons have _________ charge.",options4,"-"));
+
+        String[] options5 =  new String[]{"Lubricants","Cutting","Crafting","Nil"};
+        quizQues.add(new Quiz("Bucky Balls are used as",options5,"Lubricants"));
+
+        String[] options6 =  new String[]{"Soft","Hard","Bulky","Nil"};
+        quizQues.add(new Quiz("Diamonds is ____________",options6,"Hard"));
+
+        String[] options7 =  new String[]{"Velocity","Density","Pressure","Nil"};
+        quizQues.add(new Quiz("Mass per unit volume is:",options7,"Density"));
+
+        String[] options8 =  new String[]{"Covalent","Ionic","Intermolecular","Nil"};
+        quizQues.add(new Quiz("The forces of attraction present between the molecules of a substance are called ____________ forces.",options8,"Intermolecular"));
+
+        String[] options9 =  new String[]{"4","5","6","7"};
+        quizQues.add(new Quiz("The earth is made up of ___________ % of iron.",options9,"5"));
+
+        String[] options10 =  new String[]{"Dry Cell","Zn Cell","Chargeable Cell","None of these"};
+        quizQues.add(new Quiz("____________ is an irreversible cell.",options10,"Dry Cell"));
+
+        Collections.shuffle(quizQues);
+
+        setData(i);
+        btn1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(quizQues.get(i).getAnswer().equals(btn1.getText().toString()))
+                {
+                    score++;
+
+                }
+                setData(++i);
+            }
+        });
+
+        btn2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(quizQues.get(i).getAnswer().equals(btn2.getText().toString()))
+                {
+                    score++;
+
+                }
+                setData(++i);
+            }
+        });
+
+        btn3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(quizQues.get(i).getAnswer().equals(btn3.getText().toString()))
+                {
+                    score++;
+
+                }
+                setData(++i);
+            }
+        });
+
+        btn4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if(quizQues.get(i).getAnswer().equals(btn4.getText().toString()))
+                {
+                    score++;
+
+                }
+                setData(++i);
+            }
+        });
+
+
+
     }
+
+
+
+    private void setData(int num) {
+        que.setText(quizQues.get(num).getQue() );
+        String [] array = quizQues.get(num).getOpts();
+
+        List<String> opt = Arrays.asList(array);
+
+        Collections.shuffle(opt);
+
+        opt.toArray(array);
+
+        btn1.setText(array[0]);
+        btn2.setText(array[1]);
+        btn3.setText(array[2]);
+        btn4.setText(array[3]);
+    }
+
+
 }
--- app/src/main/java/com/example/quizapp/Quiz.java
+++ app/src/main/java/com/example/quizapp/Quiz.java
@@ -3,18 +3,12 @@ package com.example.quizapp;
 public class Quiz {
 
     public String que;
-    public String opt1;
-    public String opt2;
-    public String opt3;
-    public String opt4;
+    public String[] opts;
     public String answer;
 
-    public Quiz(String que, String opt1, String opt2, String opt3, String opt4, String answer) {
+    public Quiz(String que, String[] opts, String answer) {
         this.que = que;
-        this.opt1 = opt1;
-        this.opt2 = opt2;
-        this.opt3 = opt3;
-        this.opt4 = opt4;
+        this.opts = opts;
         this.answer = answer;
     }
 
@@ -26,36 +20,12 @@ public class Quiz {
         this.que = que;
     }
 
-    public String getOpt1() {
-        return opt1;
+    public String[] getOpts() {
+        return opts;
     }
 
-    public void setOpt1(String opt1) {
-        this.opt1 = opt1;
-    }
-
-    public String getOpt2() {
-        return opt2;
-    }
-
-    public void setOpt2(String opt2) {
-        this.opt2 = opt2;
-    }
-
-    public String getOpt3() {
-        return opt3;
-    }
-
-    public void setOpt3(String opt3) {
-        this.opt3 = opt3;
-    }
-
-    public String getOpt4() {
-        return opt4;
-    }
-
-    public void setOpt4(String opt4) {
-        this.opt4 = opt4;
+    public void setOpts(String[] opts) {
+        this.opts = opts;
     }
 
     public String getAnswer() {
